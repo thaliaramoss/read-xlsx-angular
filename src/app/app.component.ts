cd96881153a418905beefecb9b7e87a40d599e0e
Thalia <thzliaramos@gmail.com>
2023-12-03 00:46:38 -0300

    reading xlsx files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { Binary } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_xlsx';
+
+  displayedColumns: string[] = []
+  dataSource = new MatTableDataSource<any>([])
+  data: any
+
+  readExcel(event: any) {
+    let file = event.target.files[0]
+
+    let fileReader = new FileReader()
+    fileReader.readAsBinaryString(file)
+
+    fileReader.onload = (e) => {
+      let workbook = XLSX.read(fileReader.result, {type: 'binary'})
+      let sheetNames = workbook.SheetNames
+      let sheet = workbook.Sheets[sheetNames[0]]
+      let jsonData = XLSX.utils.sheet_to_json(sheet)
+
+      this.dataSource.data = jsonData
+
+      // primeira linha - coluna com os nomes das colunas
+      this.displayedColumns = Object.keys(jsonData[0] as string[]);
+
+
+      console.log(this.dataSource.data)
+    }
+  }
 }
